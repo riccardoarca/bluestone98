d85efadb829f068eed31d37bfd98b4de923aa60b
Riccardo Arca <riccardo@gmail.com>
2023-03-19 17:53:24 +0000

    footer

--- site/wp-content/themes/bluestone98-blog/footer.php
+++ site/wp-content/themes/bluestone98-blog/footer.php
@@ -1,7 +1,26 @@
 
 
+	<div class="wrapper px-0" id="wrapper-footer">
 
-	</div><!-- wrapper end -->
+		<div class="px-0 containerfull">
+
+			<div class="row">
+
+				<footer class="site-footer">
+
+					<div class="site-info pd_mob">
+						
+					</div>
+
+				</footer>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div><!-- wrapper end -->
 
 
 <?php wp_footer(); ?>
