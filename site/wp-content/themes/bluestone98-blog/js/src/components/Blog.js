c36256850f7cb59ff2e100f95637b6cdaa556f55
Riccardo Arca <riccardo@gmail.com>
2023-03-20 16:45:49 +0000

    updating JS

--- site/wp-content/themes/bluestone98-blog/js/src/components/Blog.js
+++ site/wp-content/themes/bluestone98-blog/js/src/components/Blog.js
@@ -1,10 +1,12 @@
-if (document.body.classList.contains('blog')) {
-   const waypoint = new Waypoint({
-   element: document.querySelector('.fade_in_up'),
-	  handler: function(element, direction) {
-	  	this.element.classList.add( 'animatetrigger', 'animated', 'fadeIn' );
-	  },
-	  offset: '75%'
-	});
-}
+document.addEventListener("DOMContentLoaded", () => {
+	if (document.body.classList.contains('blog')) {
+	   const waypoint = new Waypoint({
+	   element: document.querySelector('.fade_in_up'),
+		  handler: function(element, direction) {
+		  	this.element.classList.add( 'animatetrigger', 'animated', 'fadeIn' );
+		  },
+		  offset: '75%'
+		});
+	}
+});
 
